6389225d009c6de7c999e46d5031834463f0aa0c
root <root@UNIT001.(none)>
2014-06-19 17:36:19 +0800

    Added Identifiers, Lexical Structure, and Literals. I pushed kasi minamadali na ako nila lemuel

---
+++ Chapter2/Lexical_Structure.php
@@ -0,0 +1,28 @@
+<?php
+function SampleFunc(){
+	echo "<li>Function Calls</li>";
+}
+echo "Built-in Constructs <br/>";
+ECHO "and Keywords <br/>";
+ecHo "are CaSe-<b>IN</b>SensITivE <br/>";
+echo "<hr>";
+echo "PHP Statements include: <br/><ul>";
+SampleFunc();
+$assignment = "<li>An assignment</li>";
+$ifstatement = "<li>If Statement</li>";
+$a = 0;
+	if ($a === 0){
+		echo $assignment;
+		echo $ifstatement;
+	}	
+echo "</ul><hr>";
+echo "WHITESPACE and LINE BREAKS<br/>";
+echo
+"DOESN'T MATTER<br/>";
+echo	"IN PHP<br/>";
+
+
+///// 	THIS IS A C++ STYLE COMMENT 	//
+##### 	THIS IS A SHELL STYLE COMMENT 	##
+/*	THIS IS A C STYLE COMMENT	*/
+?>
