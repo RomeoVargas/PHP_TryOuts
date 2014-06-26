638af99b413a6a167e812e6fdcefc57b0e4ee51a
root <root@UNIT001.(none)>
2014-06-26 14:35:05 +0800

    Added DebuggingPHP in Chapter16, My Codes are too short because Chapter16 is more on explanation about PHP Settings and ways of debugging Codes

---
+++ Chapter16/DebuggingPHP.php
@@ -0,0 +1,7 @@
+<?php
+	error_reporting(E_ALL); // error reporting is on for all errors
+	error_reporting(0); // error reporting turned off
+	ini_set("display_errors", 1); // display errors turned on -- 1 means on 
+
+
+?>
